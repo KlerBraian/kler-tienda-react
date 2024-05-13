d9a135b4149bccdafa3c30fa2e7b1ad1fe13935f
klerbraian <braiankler.30@gmail.com>
2024-05-13 14:54:38 -0300

    q

--- src/components/ItemListCont.jsx
+++ src/components/ItemListCont.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const ItemListCont = () => {
   return (
     <div className='productos'>
-      <h1>Seccion de productos</h1>
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+      <h1 className='productos-h1'>Seccion de productos</h1>
     </div>
       
   )
